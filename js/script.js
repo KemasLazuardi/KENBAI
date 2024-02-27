5f7badec37bc764bcfbe300945a40ba5bda923c8
KemasLazuardi <110363545+KemasLazuardi@users.noreply.github.com>
2024-02-27 19:32:19 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function scrollToAbout() {
+    document.getElementById('about').scrollIntoView();
+}
+function scrollToServices() {
+    document.getElementById('services').scrollIntoView();
+}
+function scrollToProjects() {
+    document.getElementById('projects').scrollIntoView();
+}
+function scrollToTestimonials() {
+    document.getElementById('testimonials').scrollIntoView();
+}
+function scrollToContact() {
+    document.getElementById('contact').scrollIntoView();
+}
